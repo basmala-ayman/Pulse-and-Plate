e215fa5617452adc90df3bccc38c58c84c1c4a2e
Habiba Adel <158005569+Habiba-Adel@users.noreply.github.com>
2025-05-14 23:45:49 +0200

    updating the functions of the cart page

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const Order = require("../models/order.js"); 
 const Item = require("../models/item.js");   
 const User = require("../models/user.js");   
@@ -6,61 +7,95 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-//auth here cause of 
-router.post("/", auth, async (req, res) => {
-    const {itemname} = req.body; 
-    const userId = req.user._id; //here to get the number is of the user that making this function 
 
-    if (!itemname) {
-        return res.status(400).json({ message: "the item name is required." });
-    }
+// makign the get order function and returning the item info
+// now in this function the front team will send the name of the item that the user choosen it to added in the cart 
+//and will use this api to get all the information related to this item
+router.get("/item/name/:name", async (req, res) => {
+  const itemName = req.params.name;
+  try {
+    const item = await Item.findOne({ name: itemName });
+    if (!item) return res.status(404).json({ message: "Item not found" });
 
-    try {
-      const item = await Item.findOne({ name: itemname });
-        if (!item) {
-            return res.status(404).json({ message: "Item not found." });
-        }
-
-        if (item.avilableCounter <= 0) {
-            return res.status(400).json({ message: "Item is out of stock." });
-        }
-
-        const newOrder = new Order({
-            user: userId,
-            item: item._id,
-        });
-        await newOrder.save(); //saving the order in the data base
-
-        item.avilableCounter -= 1;
-        await item.save(); //and this is for saving the quantity of this item in the daa base too
-
-        const user = await User.findById(userId);//but here is to get the full object of this user 
-
-        if (!user) {
-
-            console.error("User not found after order creation, order ID:", newOrder._id);
-
-            try {
-              await Order.findByIdAndDelete(newOrder._id);
-              console.log(`Orphaned order (ID: ${newOrder._id}) deleted successfully.`);
-                } catch (deleteError) {
-              console.error(`Failed to delete orphaned order (ID: ${newOrder._id}).`, deleteError);
-                }
-          
-          return res.status(500).json({ 
-              message: "Order created but the user not found so The order has been cancelled.", 
-          });
-        }
-
-        user.ordersList.push(newOrder._id);
-        await user.save(); 
-
-        res.status(201).json({ message: "Order created successfully", order: newOrder });
-
-    } catch (error) {
-        console.error("Error creating order:", error);
-        res.status(500).json({ message: "Error creating order.", error: error.message });
-    }
+    res.status(200).json({
+      message: "Item info returned successfully",
+      item: item
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching item", error: err.message });
+  }
 });
+
+//now after the front taking the item information and showing it in the cart page they will need to send 
+//the item name again but with the number of quantites the user is ordered for it and this will happened by this 
+// we will use the aith here to ensure that the loggend users who is making this request 
+router.post("/", auth, async (req, res) => {
+  const { itemname, quantity } = req.body;
+  const userId = req.user._id;
+
+  // Basic validation
+  if (!itemname ) {
+    return res.status(400).json({
+      message: "Item name  is required.",
+    });
+  }
+
+  if ( !quantity || quantity <= 0) {
+    return res.status(400).json({
+      message: "valid quantity is required.",
+    });
+  }
+
+
+  try {
+    // first we will need to check that this item name is existed 
+    const item = await Item.findOne({ name: itemname });
+    if (!item) {
+      return res.status(404).json({ message: "Item not found." });
+    }
+
+    // second checking that the available counter of this item is less than the asked quantity 
+    if (item.avilableCounter < quantity) {
+      return res.status(400).json({
+        message: `Only ${item.avilableCounter} items are available in stock.`,
+      });
+    }
+
+    const newOrder = new Order({
+      user: userId,
+      item: item._id,
+      quantity,
+    });
+    await newOrder.save();
+
+    item.avilableCounter -= quantity;
+    await item.save();
+
+    // adding this order to the user order list 
+    const user = await User.findById(userId);
+    if (!user) {
+      await Order.findByIdAndDelete(newOrder._id);
+      return res.status(500).json({
+        message: "Order created but user not found. The order has been cancelled.",
+      });
+    }
+
+    user.ordersList.push(newOrder._id);
+    await user.save();
+
+    res.status(201).json({
+      message: "Order created successfully.",
+      order: newOrder,
+    });
+
+  } catch (error) {
+    console.error("Error processing order:", error);
+    res.status(500).json({
+      message: "Something went wrong while processing the order.",
+      error: error.message,
+    });
+  }
+});
+
 
 module.exports = router;
