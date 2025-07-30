f3b295f53b5709a013c79abd30a599de1e336969
Basmala Ayman <basmala.ayman.1904@gmail.com>
2025-07-30 13:38:44 +0300

    separate to client and server folders

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config(); // Load environment variables from .env
 
 const { swaggerUi, swaggerSpec } = require('./swagger');
@@ -14,11 +15,36 @@ const MONGO_URI = process.env.MONGODB_URI;
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://pulse-and-plate.onrender.com'
+];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use('/api/users', userRoutes);
+app.use('/api/product', productRoutes);
+app.use('/api/addorder', orderRoutes);
+
+// Health Check for deployment
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Serve React Build
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
@@ -34,7 +60,3 @@ mongoose.connect(MONGO_URI, {
   .catch((error) => {
     console.error(' Failed to connect to MongoDB Atlas:', error.message);
   });
-
-app.use('/api/users', userRoutes);
-app.use('/api/product', productRoutes);
-app.use('/api/addorder', orderRoutes);
