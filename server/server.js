071b00374cbaf8a15e04134b4d398d2ce009ef23
Basmala Ayman <basmala.ayman.1904@gmail.com>
2025-07-31 22:45:26 +0300

    update cors for server

--- server/server.js
+++ server/server.js
@@ -14,12 +14,12 @@ const MONGO_URI = process.env.MONGODB_URI;
 
 const app = express();
 
-app.use(cors());
-// app.use(cors({
-//   origin: 'https://pulse-and-plate.vercel.app',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
+// app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
