4504f934c6a729ddf57ac873084bdbeb3aae4e75
Basmala Ayman <basmala.ayman.1904@gmail.com>
2025-08-01 21:30:58 +0300

    add cloudinary to server

--- server/server.js
+++ server/server.js
@@ -14,12 +14,28 @@ const MONGO_URI = process.env.MONGODB_URI;
 
 const app = express();
 
+const cloudinary = require("cloudinary");
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({
+    path: "./.env",
+  });
+}
+
+const corsConfig = {
+  origin: process.env.ClIENT_URL,
+  credentials: true,
+  method: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig));
+
 // app.use(cors());
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }));
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
@@ -49,3 +65,11 @@ mongoose.connect(MONGO_URI, {
   .catch((error) => {
     console.error(' Failed to connect to MongoDB Atlas:', error.message);
   });
+
+
+// for deployment
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
