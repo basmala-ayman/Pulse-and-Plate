9c71be2c737745edf6cea06048580225c9d204c9
Basmala Ayman <basmala.ayman.1904@gmail.com>
2025-08-04 10:59:06 +0300

    fix cors

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
 
 const corsConfig = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
